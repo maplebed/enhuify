6ab71a6ad0299cc264434fc36a42d7b3e036f855
Sam Stokes <me@samstokes.co.uk>
2018-04-09 10:24:44 -0700

    hacky untested changes for queue-per-shard, for reference only

--- app/jobs/light_changes_job.rb
+++ app/jobs/light_changes_job.rb
@@ -1,7 +1,24 @@
 class LightChangesJob < ApplicationJob
   queue_as :default
 
+  def self.queues
+    @queues ||= [0, 1].map do |shard|
+      q = SizedQueue.new
+      Thread.new do
+        while bulbmap, changelog = q.pop
+          perform_shard(bulbmap, changelog)
+        end
+      end
+      q
+    end
+  end
+
   def perform(bulbmap, changelog)
+    shard = bulbmap.id
+    self.queues[shard] << [bulbmap, changelog]
+  end
+
+  def perform_shard(bulbmap, changelog)
     logger.info "processing queued change: #{bulbmap}"
     bulb = Bulb.find(bulbmap["id"])
     # bulb.assign_attributes({
