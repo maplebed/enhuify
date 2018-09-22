de6d0e5ca9e08f00c682a214a94373adbea6893d
Shane Becker <veganstraightedge@gmail.com>
2018-09-22 16:15:10 -0700

    Rubocop autofix app/jobs/light_changes_job.rb

--- app/jobs/light_changes_job.rb
+++ app/jobs/light_changes_job.rb
@@ -4,7 +4,7 @@ class LightChangesJob < ApplicationJob
   queue_as :default
 
   def self.queues
-    @queues ||= [0,1].map do |shard|
+    @queues ||= [0, 1].map do |_shard|
       q = Queue.new
       Thread.new do
         while job = q.pop
@@ -12,7 +12,7 @@ class LightChangesJob < ApplicationJob
           begin
             # don't let job panics kill the thread
             perform_shard(bulbmap, changelog, q.length)
-          rescue => e
+          rescue StandardError => e
             logger.error "caught exception: #{e}, #{e.backtrace}"
           end
         end
@@ -27,8 +27,8 @@ class LightChangesJob < ApplicationJob
     LightChangesJob.queues[shard] << [bulbmap, changelog]
   end
 
-  def self.clear()
-    logger.info "clearing queue for both shards"
+  def self.clear
+    logger.info 'clearing queue for both shards'
     LightChangesJob.queues[0].clear
     LightChangesJob.queues[1].clear
   end
@@ -37,16 +37,16 @@ class LightChangesJob < ApplicationJob
     @honeycomb ||= Libhoney::Client.new(
       # Use an environment variable to set your write key with something like
       #   `:writekey => ENV["HONEYCOMB_WRITEKEY"]`
-      :writekey =>  ENV["HONEYCOMB_WRITEKEY"],
-      :dataset => "enhuify"
+      writekey: ENV['HONEYCOMB_WRITEKEY'],
+      dataset: 'enhuify'
     )
   end
 
   def self.perform_shard(bulbmap, changelog, qlen)
-    ev = honeycomb.event()
+    ev = honeycomb.event
     ev.timestamp = changelog.created_at
     ev.add(bulbmap)
-    ev.add_field("queue_length", qlen)
+    ev.add_field('queue_length', qlen)
 
     # mark this job as having been processed, no longer pending
     changelog.processed = true
@@ -57,20 +57,20 @@ class LightChangesJob < ApplicationJob
       start = Time.now
       ok = bulb.save
       dur = Time.now - start
-      ev.add_field("saveSec", dur)
+      ev.add_field('saveSec', dur)
       if ok == true
         changelog.succeeded = true
       else
-        logger.error "failed to save bulb for unknown reason"
+        logger.error 'failed to save bulb for unknown reason'
       end
-    rescue => e
+    rescue StandardError => e
       logger.error "bulb save blew up: #{e}, #{e.backtrace}"
     end
     changelog.save!
 
     ev.add(changelog.attributes)
-    ev.add_field("delaySec", changelog.updated_at - changelog.created_at )
-    ev.send()
+    ev.add_field('delaySec', changelog.updated_at - changelog.created_at)
+    ev.send
 
     # take a while to finish the job to limit the speed at which jobs are pulled
     # off the queue.  Rate set via config
